9abbeeb6031c0a33ec75e798a08ec86f49101e28
krylovit <krylovit@gmail.com>
2025-12-12 13:17:59 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,8 +3,8 @@ const CONFIG = {
     INITIAL_BALANCE: 1000.0,
     DEFAULT_BET: 10,
     LOOKBACK: 50,
-    UPDATE_INTERVAL: 30000, // 30 секунд для тестирования
+    UPDATE_INTERVAL: 30000, // 30 секунд
     API_URL: 'https://api.binance.com/api/v3/klines',
     FEATURES: 8,
-    MODEL_KEY: 'neuro_trader_lstm_model_v3'
+    MODEL_KEY: 'neuro_trader_lstm_model_v4'
 };
